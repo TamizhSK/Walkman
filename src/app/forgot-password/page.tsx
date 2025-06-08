05833d9c42c2708fea95ff65079bd087503b1436
TamizhSK <tamizhsk1@gmail.com>
2025-06-08 13:47:09 +0530

    chore : minor changes

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -118,15 +118,17 @@ export default function ForgotPassword() {
           </div>
         </div>
 
-        {/* Background Image for Desktop */}
-        <div className="relative hidden lg:block">
-          <img
-            src="https://res.cloudinary.com/dqcf0a6dk/image/upload/v1747025733/img4_pczl7x.jpg"
-            alt="Forgot password background"
-            className="absolute inset-0 h-full w-full object-cover"
-          />
-          <div className="absolute inset-0 bg-black/20" />
-        </div>
+      {/* Background Image for Desktop */}
+      <div className="relative hidden lg:block">
+        <img
+          src="https://res.cloudinary.com/dqcf0a6dk/image/upload/v1747025733/img4_pczl7x.jpg"
+          alt="Forgot password background"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black/20" />
+        {/* Gradient blend overlay */}
+        <div className="absolute left-0 top-0 h-full w-92 bg-gradient-to-r from-neutral-950 to-transparent z-10" />
+      </div>
       </div>
     );
   }
