ab411782296d181acf73851553c15ec344142210
TamizhSK <tamizhsk1@gmail.com>
2025-06-08 16:46:11 +0530

    feat : Custom Pointer

--- src/components/magicui/pointer.tsx
+++ src/components/magicui/pointer.tsx
@@ -7,7 +7,7 @@ import {
   motion,
   useMotionValue,
 } from "motion/react";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface PointerProps extends Omit<HTMLMotionProps<"div">, "ref"> {}
 
@@ -24,7 +24,7 @@ export function Pointer({
   style,
   children,
   ...props
-}: PointerProps): JSX.Element {
+}: PointerProps): React.ReactElement {
   const x = useMotionValue(0);
   const y = useMotionValue(0);
   const [isActive, setIsActive] = useState<boolean>(false);
