71a857b029a793016691a341d4feab226259d6df
TamizhSK <tamizhsk1@gmail.com>
2025-06-10 00:03:49 +0530

    chore : UI Changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -239,7 +239,7 @@ export default function Nav() {
               </div>
               <div className="w-full space-y-2">
                 <Link href="/profile" onClick={() => setIsOpen(false)} className="w-full">
-                  <Button className="w-full mb-2 rounded-4xl bg-zinc-700 hover:bg-zinc-600 text-white">
+                  <Button className="w-full mb-2 rounded-4xl text-base bg-zinc-700 hover:bg-zinc-600 text-white">
                     <User className="mr-2 h-4 w-4" />
                     Profile
                   </Button>
@@ -249,7 +249,7 @@ export default function Nav() {
                     handleLogout();
                     setIsOpen(false);
                   }}
-                  className="w-full rounded-4xl bg-red-600 hover:bg-red-700 text-white"
+                  className="w-full rounded-4xl text-base bg-red-600 hover:bg-red-700 text-white"
                 >
                   <LogOut className="mr-2 h-4 w-4" />
                   Log out
