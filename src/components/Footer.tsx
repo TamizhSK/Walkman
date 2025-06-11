41f097454f10ff54ececba064938e8d595baa45d
TamizhSK <tamizhsk1@gmail.com>
2025-06-11 13:39:11 +0530

    chore : minor updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -177,6 +177,11 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      <div className="mt-4 text-[10px] text-gray-700 text-center">
+        <p>
+          Disclaimer: All media and images displayed are for development purposes only. No copyright infringement is intended.
+        </p>
+      </div>
     </footer>
   );
 }
